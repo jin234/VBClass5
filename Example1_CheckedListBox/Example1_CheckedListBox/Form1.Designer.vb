35ad30430576d49c28ebb0c09413d8a4eaf637dd
jirapat kanokmaneeporn <ka.jirapat@gmail.com>
2018-12-11 14:16:32 +0700

    Add Example

---
+++ Example1_CheckedListBox/Example1_CheckedListBox/Form1.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CheckedListBox = New System.Windows.Forms.CheckedListBox()
+        Me.TextBoxItem = New System.Windows.Forms.TextBox()
+        Me.ButtonAdd = New System.Windows.Forms.Button()
+        Me.ButtonDelete = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'CheckedListBox
+        '
+        Me.CheckedListBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckedListBox.FormattingEnabled = True
+        Me.CheckedListBox.Location = New System.Drawing.Point(247, 30)
+        Me.CheckedListBox.Name = "CheckedListBox"
+        Me.CheckedListBox.Size = New System.Drawing.Size(272, 179)
+        Me.CheckedListBox.TabIndex = 0
+        '
+        'TextBoxItem
+        '
+        Me.TextBoxItem.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxItem.Location = New System.Drawing.Point(24, 30)
+        Me.TextBoxItem.Name = "TextBoxItem"
+        Me.TextBoxItem.Size = New System.Drawing.Size(197, 30)
+        Me.TextBoxItem.TabIndex = 1
+        '
+        'ButtonAdd
+        '
+        Me.ButtonAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonAdd.Location = New System.Drawing.Point(24, 74)
+        Me.ButtonAdd.Name = "ButtonAdd"
+        Me.ButtonAdd.Size = New System.Drawing.Size(90, 41)
+        Me.ButtonAdd.TabIndex = 2
+        Me.ButtonAdd.Text = "Add"
+        Me.ButtonAdd.UseVisualStyleBackColor = True
+        '
+        'ButtonDelete
+        '
+        Me.ButtonDelete.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonDelete.Location = New System.Drawing.Point(131, 74)
+        Me.ButtonDelete.Name = "ButtonDelete"
+        Me.ButtonDelete.Size = New System.Drawing.Size(90, 41)
+        Me.ButtonDelete.TabIndex = 3
+        Me.ButtonDelete.Text = "Delete"
+        Me.ButtonDelete.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(550, 240)
+        Me.Controls.Add(Me.ButtonDelete)
+        Me.Controls.Add(Me.ButtonAdd)
+        Me.Controls.Add(Me.TextBoxItem)
+        Me.Controls.Add(Me.CheckedListBox)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CheckedListBox As CheckedListBox
+    Friend WithEvents TextBoxItem As TextBox
+    Friend WithEvents ButtonAdd As Button
+    Friend WithEvents ButtonDelete As Button
+End Class
---
+++ Example1_CheckedListBox/Example4_PictureBox/Form1.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBoxCard = New System.Windows.Forms.PictureBox()
+        Me.rdCard1 = New System.Windows.Forms.RadioButton()
+        Me.rdCard2 = New System.Windows.Forms.RadioButton()
+        Me.rdCard3 = New System.Windows.Forms.RadioButton()
+        Me.rdCard4 = New System.Windows.Forms.RadioButton()
+        CType(Me.PictureBoxCard, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBoxCard
+        '
+        Me.PictureBoxCard.Location = New System.Drawing.Point(39, 41)
+        Me.PictureBoxCard.Name = "PictureBoxCard"
+        Me.PictureBoxCard.Size = New System.Drawing.Size(226, 267)
+        Me.PictureBoxCard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBoxCard.TabIndex = 0
+        Me.PictureBoxCard.TabStop = False
+        '
+        'rdCard1
+        '
+        Me.rdCard1.AutoSize = True
+        Me.rdCard1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rdCard1.Location = New System.Drawing.Point(292, 41)
+        Me.rdCard1.Name = "rdCard1"
+        Me.rdCard1.Size = New System.Drawing.Size(102, 29)
+        Me.rdCard1.TabIndex = 1
+        Me.rdCard1.TabStop = True
+        Me.rdCard1.Text = "ไพ่โพธิ์ดำ"
+        Me.rdCard1.UseVisualStyleBackColor = True
+        '
+        'rdCard2
+        '
+        Me.rdCard2.AutoSize = True
+        Me.rdCard2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rdCard2.Location = New System.Drawing.Point(292, 86)
+        Me.rdCard2.Name = "rdCard2"
+        Me.rdCard2.Size = New System.Drawing.Size(111, 29)
+        Me.rdCard2.TabIndex = 2
+        Me.rdCard2.TabStop = True
+        Me.rdCard2.Text = "ไพ่โพธิ์แดง"
+        Me.rdCard2.UseVisualStyleBackColor = True
+        '
+        'rdCard3
+        '
+        Me.rdCard3.AutoSize = True
+        Me.rdCard3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rdCard3.Location = New System.Drawing.Point(292, 134)
+        Me.rdCard3.Name = "rdCard3"
+        Me.rdCard3.Size = New System.Drawing.Size(103, 29)
+        Me.rdCard3.TabIndex = 3
+        Me.rdCard3.TabStop = True
+        Me.rdCard3.Text = "ไพ่ดอกจิก"
+        Me.rdCard3.UseVisualStyleBackColor = True
+        '
+        'rdCard4
+        '
+        Me.rdCard4.AutoSize = True
+        Me.rdCard4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rdCard4.Location = New System.Drawing.Point(292, 180)
+        Me.rdCard4.Name = "rdCard4"
+        Me.rdCard4.Size = New System.Drawing.Size(142, 29)
+        Me.rdCard4.TabIndex = 4
+        Me.rdCard4.TabStop = True
+        Me.rdCard4.Text = "ไพ่ข้าวหลามตัด"
+        Me.rdCard4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(467, 344)
+        Me.Controls.Add(Me.rdCard4)
+        Me.Controls.Add(Me.rdCard3)
+        Me.Controls.Add(Me.rdCard2)
+        Me.Controls.Add(Me.rdCard1)
+        Me.Controls.Add(Me.PictureBoxCard)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBoxCard, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBoxCard As PictureBox
+    Friend WithEvents rdCard1 As RadioButton
+    Friend WithEvents rdCard2 As RadioButton
+    Friend WithEvents rdCard3 As RadioButton
+    Friend WithEvents rdCard4 As RadioButton
+End Class
